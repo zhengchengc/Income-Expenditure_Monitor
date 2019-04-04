fd9d99e96c6be6313396b8ce0278db043ec751d9
olichid <zhengchengc@student.unimelb.edu.au>
2019-04-04 22:00:25 +1100

    initialize the server-side

---
+++ server-side/src/main/java/com/olichid/moneyMonitor/APIforMoneyMonitor/ApIforMoneyMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.olichid.moneyMonitor.APIforMoneyMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIforMoneyMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIforMoneyMonitorApplication.class, args);
+	}
+
+}
---
+++ server-side/src/test/java/com/olichid/moneyMonitor/APIforMoneyMonitor/ApIforMoneyMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olichid.moneyMonitor.APIforMoneyMonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApIforMoneyMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
